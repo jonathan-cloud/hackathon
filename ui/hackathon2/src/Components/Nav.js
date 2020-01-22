da8b975e2adbccf4f1c66179e780ab9081b72b06
daniel.oren <orendaniel92@gmail.com>
2020-01-22 18:50:20 +0200

    Before correcting folder duplications

--- ui/hackathon2/src/Components/Nav.js
+++ ui/hackathon2/src/Components/Nav.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Navbar, Form, FormControl, Button } from 'react-bootstrap'
+// import ui/hackathon2/node_modules/bootstrap/dist/css/bootstrap.min.css
 const Nav = () => {
   return (
     <div className="container-fluid">
       <div className="row-fluid">
         <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-
-          <Form inline>
+          <Navbar.Brand href="#home">WeAct</Navbar.Brand>
+          {/* <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
             <Button variant="outline-info">Search</Button>
-          </Form>
+          </Form> */}
         </Navbar>
       </div>
     </div>
