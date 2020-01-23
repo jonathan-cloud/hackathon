16eaf32c77ab04fe35c2ec200f3374eabb438109
Gidon Weil <Gidon.we@gmail.com>
2020-01-23 15:52:46 +0200

    geopy

--- data/db_setup.py
+++ data/db_setup.py
@@ -3,10 +3,18 @@ import csv
 import pandas as pd
 import numpy as np
 
+from geopy.geocoders import Nominatim
 from data.utils import db_connection
-
+from tqdm import tqdm
+from geopy.exc import GeocoderTimedOut
 
 CONF_PATH = '../db_config.json'
+
+def do_geocode(record, geolocator):
+    try:
+        return geolocator.geocode(record)
+    except GeocoderTimedOut:
+        return do_geocode(record, geolocator)
 
 
 def create_db():
@@ -23,8 +31,8 @@ def create_db():
         );''')
         cur.execute(f'''CREATE TABLE IF NOT EXISTS locations (
                         address varchar(255) PRIMARY KEY,
-                        lon float,
-                        lat float
+                        lat float,
+                        lon float
         );''')
         cur.execute(f'''CREATE TABLE IF NOT EXISTS types_to_locations (
                         id int AUTO_INCREMENT PRIMARY KEY, 
@@ -37,8 +45,8 @@ def create_db():
 
         cur.execute(f'''CREATE TABLE IF NOT EXISTS history (
                         id int AUTO_INCREMENT PRIMARY KEY,
-                        lon float,
                         lat float,
+                        lon float,
                         type_id int,
 
                         FOREIGN KEY (type_id) REFERENCES types(id)
@@ -52,15 +60,21 @@ def insert(file, his_data):
     dbname = db_config['db_name']
     street = set(df1.address)
     types = set(df1.type)
+    geolocator = Nominatim(user_agent="recycling")
 
     with db_connection(**db_config['user_conf']) as con:
         cur = con .cursor()
         cur.execute(f"USE {dbname}")
         # locations table
-        for record in street:
-            lan, lon = np.random.uniform(low=32, high=35, size=2)
-            insert_query = """INSERT INTO locations (address, lon, lat) VALUES (%s, %s, %s)"""
-            cur.execute(insert_query, (record, lan, lon))
+        for record in tqdm(street):
+            location = do_geocode(record, geolocator)
+            if location is None:
+                lat = np.random.uniform(32.0496, 32.0959)
+                lon = np.random.uniform(34.7740, 34.7988)
+            else:
+                lat, lon = location.latitude, location.longitude
+            insert_query = """INSERT INTO locations (address, lat, lon) VALUES (%s, %s, %s)"""
+            cur.execute(insert_query, (record, lat, lon))
 
         # types table
         for record in types:
@@ -81,9 +95,5 @@ def insert(file, his_data):
             f.readline()
             reader = csv.reader(f, delimiter=',')
             for lat, lon, c_type in reader:
-                insert_query = """INSERT INTO history (lon, lat, type_id) VALUES (%s, %s, %s)"""
+                insert_query = """INSERT INTO history (lat, lon, type_id) VALUES (%s, %s, %s)"""
                 cur.execute(insert_query, (lat, lon, c_type))
-
-
-create_db()
-insert('hackathon_data.xlsx', 'history.csv')
