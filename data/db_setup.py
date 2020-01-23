387efcde455842c4c169c94d0913d11629f6fdc8
olga <hazin.olga@gmailcom>
2020-01-23 10:13:39 +0200

    inset to db

--- data/db_setup.py
+++ data/db_setup.py
@@ -27,15 +27,50 @@ def create_db():
                         FOREIGN KEY (location_address) REFERENCES locations(address),
                         FOREIGN KEY (type_id) REFERENCES types(id)
         );''')
+
         cur.execute(f'''CREATE TABLE IF NOT EXISTS history (
-                        id int AUTO_INCREMENT PRIMARY KEY, 
+                        id int AUTO_INCREMENT PRIMARY KEY,
                         lan float,
-                        lat float
+                        lat float,
                         type_id int,
-                        
+
                         FOREIGN KEY (type_id) REFERENCES types(id)
         );''')
 
 
 def insert(file):
-    pass
+    import pandas as pd
+    import numpy as np
+
+    xl = pd.ExcelFile(file)
+    df1 = xl.parse('data')
+
+
+    db_config = json.load(open('../db_config.json', 'r'))
+    dbname = db_config['db_name']
+    street = set(df1.address)
+    types = set(df1.type)
+
+    with db_connection(**db_config['user_conf']) as con:
+        cur = con .cursor()
+        cur.execute(f"USE {dbname}")
+        for record in street:
+            lan, lon = np.random.uniform(low=32, high=35, size=2)
+            insert_query = """INSERT INTO locations (address, lan, lat) VALUES (%s, %s, %s)"""
+            cur.execute(insert_query, (record, lan, lon))
+
+        for record in types:
+            insert_query = """INSERT INTO types (category) VALUES (%s)"""
+            cur.execute(insert_query, (record,))
+
+        cur.execute("SELECT * FROM types")
+        result = cur.fetchall()
+        dct = {k: v for v, k in result}
+
+        for index, row in df1.iterrows():
+            insert_query = """INSERT INTO types_to_locations (location_address, type_id) VALUES (%s, %s)"""
+            cur.execute(insert_query, (row[1], dct[row[0]]))
+
+
+
+# insert('hackathon_data.xlsx')
--- data/server.py
+++ data/server.py
@@ -6,31 +6,50 @@ from data.utils import db_connection
 
 app = Flask(__name__)
 
+DB_CONF = json.load(open('../db_config.json', 'r'))
+DBNAME = DB_CONF['db_name']
+
 
 # @app.route('/get_recycling_facility/<string:types>')
 def get_recycling_facility(req):
     d = json.loads(req)
-    # print(d)
     update_history(d)
     return get_air_distance(d)
 
 
-def get_air_distance(lan, lon, types):
+def get_air_distance(data):
     db_config = json.load(open('../db_config.json', 'r'))
     with db_connection(**db_config) as con:
         cur = con.cursor()
-        cur.execute(f'use {dbname}')
+        cur.execute(f'use {DBNAME}')
 
 
-def update_history(lan, lon, types):
-    pass
+
+
+
+
+
+def update_history(data):
+    with db_connection(**DB_CONF['user_conf']) as con:
+        cur = con.cursor()
+        # get types
+        cur.execute(f"USE {DBNAME}")
+        cur.execute("SELECT * FROM types")
+        result = cur.fetchall()
+        dct = {k: v for v, k in result}
+        for record in data['types']:
+            insert_query = """INSERT INTO history (lan, lat, type_id) VALUES (%s, %s, %s)"""
+            cur.execute(insert_query, (data['lan'], data['lon'], dct[record]))
+
+
 
 
 #################################################
-data= {
-  "lan": "root",
-  "lon": "localhost",
+jdata= {
+  "lan": 77.32,
+  "lon": 89.36,
   "types": "paper, dress"}
 
-st = json.dumps(data)
+st = json.dumps(jdata)
 get_recycling_facility(st)
+
